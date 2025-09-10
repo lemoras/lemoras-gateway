69f7ea18e095d08db78f76867c7d5afb83e2cfe3
onxorg <onxorg@proton.me>
2025-09-10 11:34:01 +0300

    bugfix port

--- main.go
+++ main.go
@@ -404,7 +404,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "80"
 	}
 	log.Printf("Server listening on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
